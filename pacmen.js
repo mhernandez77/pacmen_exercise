ca801cd0ddcc25e13b684927e464ae97ca5c008e
Maleni A Hernandez <maleni.alexis@gmail.com>
2022-05-16 15:36:46 -0500

    first commit

---
+++ pacmen.js
@@ -0,0 +1,77 @@
+let pos = 0;
+const pacArray = [
+    ['./images/PacMan1.png', './images/PacMan2.png'],
+    ['./images/PacMan3.png', './images/PacMan4.png']
+];
+let direction = 0;
+const pacMen = []; // This array holds all the pacmen
+
+function setToRandom(scale) {
+    return {
+        x: Math.random() * scale,
+        y: Math.random() * scale
+    }
+}
+// Factory to make a PacMan at a random position with random velocity
+function makePac() {
+    // returns an object with random values scaled {x: 33, y: 21}
+    let velocity = setToRandom(10); // {x:?, y:?}
+    let position = setToRandom(200);
+    // Add image to div id = game
+    let game = document.getElementById('game');
+    let newimg = document.createElement('img');
+    newimg.style.position = 'absolute';
+    newimg.src = './images/PacMan1.png';
+    newimg.width = 100;
+    newimg.style.left = position.x;
+    newimg.style.top = position.y;
+    game.appendChild(newimg);
+    // return details in an object
+    return {
+        position,
+        velocity,
+        newimg
+    };
+}
+
+function update() {
+    //loop over pacmen array and move each one and move image in DOM
+    pacMen.forEach((item) => {
+        checkCollisions(item)
+        item.position.x += item.velocity.x;
+        item.position.y += item.velocity.y;
+
+        item.newimg.style.left = item.position.x;
+        item.newimg.style.top = item.position.y;
+    })
+    setTimeout(update, 20);
+}
+
+function checkCollisions(item) {
+    if (item.position.x + item.velocity.x + item.newimg.width > window.innerWidth ||
+        item.position.x + item.velocity.x < 0) item.velocity.x = -item.velocity.x;
+    if (item.position.y + item.velocity.y + item.newimg.height > window.innerHeight ||
+        item.position.y + item.velocity.y < 0) item.velocity.y = -item.velocity.y;
+}
+    //
+    // detect collision with all walls and make pacman bounce
+    //
+//     let container = document.getElementById('canvas');
+//     container.offsetWidth =  80+"%";
+//     container.offsetHeight = 85+"%";
+//     let canvasWidth = Number(container.offsetWidth);
+//     let canvasHeight = Number(container.offsetHeight);
+//     let canvasTop = 0;
+//     let canvasLeft = 0;
+//     let pacMenLtR = item.position.x + item.velocity.x + item.newimg.width;
+//     let pacMenUtB = item.position.y + item.velocity.y + item.newimg.height;
+//
+//     if ( pacMenLtR >= canvasWidth || item.position.x + item.velocity.x < canvasLeft) return item.velocity.x = -item.velocity.x;
+//
+//     if ( pacMenUtB >= canvasHeight || item.position.y + item.velocity.y < canvasTop) return item.velocity.y = -item.velocity.y;
+//
+// }
+
+function makeOne() {
+    pacMen.push(makePac()); // add a new PacMan
+}
